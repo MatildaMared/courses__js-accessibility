0cd710213dd216398a5bf059d83053da6c33207f
Matilda Mared <matildamared@live.se>
2022-09-22 10:02:58 +0200

    create a more accessible dropdown component

--- src/components/better/dropdown.js
+++ src/components/better/dropdown.js
@@ -1,15 +1,68 @@
-import React, { useState, useRef, useEffect } from "react"
-import uuid from "uuid"
+import React, { useState, useRef, useEffect } from "react";
+import uuid from "uuid";
 
-import "./dropdown.scss"
+import "./dropdown.scss";
 
-const Dropdown = ({ }) => {
-    return (
-        <div
-            className="dropdown-wrap"
-        >
+const Dropdown = ({ activatorText = "Dropdown", items = [] }) => {
+	const activatorRef = useRef(null);
+	const dropdownListRef = useRef(null);
+	const [isOpen, setIsOpen] = useState(false);
 
-        </div>
-    )
-}
-export default Dropdown
+	const clickHandler = (event) => {
+		setIsOpen(!isOpen);
+	};
+
+	const keyHandler = (event) => {
+		if (event.key === "Escape" && isOpen) {
+			setIsOpen(false);
+		}
+	};
+
+	const clickOutsideHandler = (event) => {
+		if (
+			dropdownListRef.current.contains(event.target) ||
+			activatorRef.current.contains(event.target)
+		) {
+			return;
+		}
+
+		setIsOpen(false);
+	};
+
+	useEffect(() => {
+		if (isOpen) {
+			dropdownListRef.current.querySelector("a").focus();
+			document.addEventListener("mousedown", clickOutsideHandler);
+		} else {
+			document.removeEventListener("mousedown", clickOutsideHandler);
+		}
+	}, [isOpen]);
+
+	return (
+		<div className="dropdown-wrap" onKeyUp={keyHandler}>
+			<button
+				aria-haspopup="true"
+				aria-controls="dropdown1"
+				onClick={clickHandler}
+				ref={activatorRef}
+				className="dropdown-activator"
+			>
+				{activatorText}
+			</button>
+			<ul
+				id="dropdown1"
+				ref={dropdownListRef}
+				className={`dropdown-itemList ${isOpen ? "active" : ""}`}
+			>
+				{items.map((item, index) => {
+					return (
+						<li key={index}>
+							<a href={item.url}>{item.text}</a>
+						</li>
+					);
+				})}
+			</ul>
+		</div>
+	);
+};
+export default Dropdown;
